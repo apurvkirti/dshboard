1e924692260781a6c436f81e022582cdfaee8623
Aman chauhan <chauhan.aman.2244@gmail.com>
2023-01-21 15:38:00 +0530

    2nd commit

--- src/components/read.js
+++ src/components/read.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Icon, Table, Button } from "semantic-ui-react";
+import { Icon, Table, Button, Dropdown } from "semantic-ui-react";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Read() {
+  const options = [
+    { key: "option1", text: "Not Yet started", value: "0" },
+    { key: "option2", text: "Work in Progress", value: "1" },
+    { key: "option3", text: "Work Completed", value: "2" },
+  ];
 
   const [APIData, setAPIData] = useState([]);
   useEffect(() => {
@@ -23,7 +28,7 @@ export default function Read() {
     localStorage.setItem("val1", val1);
     localStorage.setItem("val2", val2);
     localStorage.setItem("val3", val3);
-    
+
     // console.log(data);
   };
 
@@ -36,7 +41,30 @@ export default function Read() {
     } else if (val === "2") {
       color = "blue";
     }
-    return <Icon color={color} name="check circle outline" size="large" />
+    return <Icon color={color} name="check circle outline" size="large" />;
+  }
+  function handleChange(id, value, val) {
+    // Update the cell in the table
+    setAPIData((prevData) =>
+      prevData.map((item) => {
+        if (item.id === id) {
+          item[val] = value;
+        }
+        return item;
+      })
+    );
+
+    // Update the backend using axios
+    axios
+      .put(`https://63c94eb3904f040a965b22d7.mockapi.io/fakenew/${id}`, {
+        [val]: value,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
@@ -45,12 +73,12 @@ export default function Read() {
         <Table.Row>
           <Table.HeaderCell rowSpan="2">ITI Name</Table.HeaderCell>
           <Table.HeaderCell rowSpan="2">Cluster</Table.HeaderCell>
-          
-          <Table.HeaderCell textAlign="center" colSpan="13">
+
+          <Table.HeaderCell textAlign="center" colSpan="3">
             Workshop
           </Table.HeaderCell>
           <Table.HeaderCell>Update</Table.HeaderCell>
-            <Table.HeaderCell>Delete</Table.HeaderCell>
+          <Table.HeaderCell>Delete</Table.HeaderCell>
         </Table.Row>
 
         <Table.Row>
@@ -68,20 +96,49 @@ export default function Read() {
               <Table.Cell>{data.Cluster}</Table.Cell>
 
               <Table.Cell textAlign="center">
-               
-              {renderIcon(data.val1)}
+          
+                  <Dropdown
+                    options={options}
+                    value= {data.val1}
+                    onChange={(e, { value }) => {
+                      if (window.confirm("Are you sure you want to update the progress?")) {
+                          handleChange(data.id, value, 'val1');
+                      }
+                  }}
+                    
+                  />
+                  {renderIcon(data.val1)}
+                
               </Table.Cell>
               <Table.Cell textAlign="center">
-               {renderIcon(data.val2)}
+              <Dropdown
+                    options={options}
+                    value= {data.val2}
+                    onChange={(e, { value }) => {
+                      if (window.confirm("Are you sure you want to update the progress?")) {
+                          handleChange(data.id, value, 'val2');
+                      }
+                  }}
+                  />
+                  {renderIcon(data.val2)}
               </Table.Cell>
               <Table.Cell textAlign="center">
-              {renderIcon(data.val3)}
+              <Dropdown
+                    options={options}
+                    value= {data.val3}
+                    onChange={(e, { value }) => {
+                      if (window.confirm("Are you sure you want to update the progress?")) {
+                          handleChange(data.id, value, 'val3');
+                      }
+                  }}
+                  />
+                  {renderIcon(data.val3)}
               </Table.Cell>
               <Link to="/update">
-                  <Table.Cell>
-                    <Button onClick={() => setData(data)}>Update</Button>
-                  </Table.Cell>
-                </Link>
+                <Table.Cell>
+                  <Button onClick={() => setData(data)}>Update</Button>
+                </Table.Cell>
+              </Link>
             </Table.Row>
           );
         })}
