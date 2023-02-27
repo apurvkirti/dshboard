5abdb9372be36d4cb9b62d3f6c69532f4b5c57e9
Fieryrival <ankitkumrmi007@gmail.com>
2023-02-27 13:27:31 +0530

    pdf baak button

--- src/components/pdfButton.js
+++ src/components/pdfButton.js
@@ -1,9 +1,8 @@
-import ReactToPrint from 'react-to-print';
-import DataComponent from './DataComponent';
-import { useNavigate } from 'react-router-dom';
+import ReactToPrint from "react-to-print";
+import DataComponent from "./DataComponent";
+import { useNavigate } from "react-router-dom";
 import React, { useEffect } from "react";
-import {Button} from 'react-bootstrap';
-
+import { Button } from "react-bootstrap";
 
 function withAuth(Component) {
   return function AuthenticatedComponent(props) {
@@ -12,25 +11,25 @@ function withAuth(Component) {
 
     useEffect(() => {
       if (!token) {
-        navigate('/');
+        navigate("/");
       }
     }, [navigate, token]);
 
     if (!token) {
       return null;
     }
+
     return <Component {...props} />;
   };
 }
 
 class PdfComponent extends React.Component {
-  
   constructor(props) {
     super(props);
     this.state = {
       timer: 15,
-      message: 'Loading data please wait for... 15 seconds',
-      buttonDisabled: true
+      message: "Loading data please wait for... 15 seconds",
+      buttonDisabled: true,
     };
   }
 
@@ -38,9 +37,12 @@ class PdfComponent extends React.Component {
     this.timerInterval = setInterval(() => {
       if (this.state.timer === 0) {
         clearInterval(this.timerInterval);
-        this.setState({ message: '', buttonDisabled: false });
+        this.setState({ message: "", buttonDisabled: false });
       } else {
-        this.setState(prevState => ({ timer: prevState.timer - 1, message: `Loading data please wait for... ${prevState.timer} seconds` }));
+        this.setState((prevState) => ({
+          timer: prevState.timer - 1,
+          message: `Loading data please wait for... ${prevState.timer} seconds`,
+        }));
       }
     }, 1000);
   }
@@ -48,18 +50,24 @@ class PdfComponent extends React.Component {
   componentWillUnmount() {
     clearInterval(this.timerInterval);
   }
-  
-
+  handleGoBack = () => {
+    window.history.back();
+  };
   render() {
-   
     return (
       <div>
         <ReactToPrint
           content={() => this.componentRef}
-          trigger={() => <Button className="btt" disabled={this.state.buttonDisabled}>Print to PDF!</Button>}
+          trigger={() => (
+            <Button className="btt" disabled={this.state.buttonDisabled}>
+              Print to PDF!
+            </Button>
+          )}
         />
-        <div className='mesgg'>{this.state.message}</div>
-        <Button className='home-button' >Go Back!</Button>
+        <div className="mesgg">{this.state.message}</div>
+        <Button className="home-button" onClick={this.handleGoBack}>
+          Go Back!
+        </Button>
         <div className="inv">
           <DataComponent ref={(response) => (this.componentRef = response)} />
         </div>
